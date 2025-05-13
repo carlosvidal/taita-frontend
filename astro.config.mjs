8d5cdd7ffaac94aac1ce8c9752f994468394b021
Carlos Vidal <carlos@mitienda.pe>
2025-05-13 15:04:33 -0500

    Configurar frontend como sitio estático para Render.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  experimental: {
-    // Habilitar la bandera experimental de sesiones
-    session: true
-  },
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
+  // No necesitamos un adaptador para sitios estáticos
+  // No necesitamos la bandera experimental de sesiones para sitios estáticos
 });
