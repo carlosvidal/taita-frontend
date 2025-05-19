eb2bc5692831afacac49f8127dcb26248cbcf3cb
Carlos Vidal <carlos@mitienda.pe>
2025-05-18 20:04:58 -0500

    fix front

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,31 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  site: 'https://demo.taita.blog',
+  output: 'static',
   adapter: node({
     mode: 'standalone',
   }),
   experimental: {
     session: true
+  },
+  // Configuración de la API base
+  server: {
+    host: true,
+    port: 4321
+  },
+  vite: {
+    define: {
+      'process.env': process.env
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://taita-api.onrender.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api')
+        }
+      }
+    }
   }
 });
